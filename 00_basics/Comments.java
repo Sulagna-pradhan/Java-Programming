58a08bb5f5601651cb5fe1b7556d107c8b062d51
Sulagna Pradhan <infinityresources81@gmail.com>
2025-06-17 12:04:18 +0530

    Changes to be committed:

---
+++ 00_basics/Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+  public static void main (String[] args){
+    System.out.print ("Hello World");
+
+    // This is single line commments in java
+     /*This 
+      is 
+      multiline 
+      comments
+      in java
+      */
+  }
+}
